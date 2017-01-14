2e83e11a26c3b134099232754e112445db841812
daniel4x <daniel4x@gmail.com>
2017-01-14 21:39:45 +0200

    commented the main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from PcapReader import PcapReader
-import networkx as nx
-from NetworkDrawer import NetworkDrawer
+# from PcapReader import PcapReader
+# import networkx as nx
+# from NetworkDrawer import NetworkDrawer
 
 
-if __name__ == '__main__':
-    reader = PcapReader('test-data/android-Sun-Dec-25-10-42-29-GMT+02-00-2016.cap')
-    # ip_pkts = reader.pkt_by_ip('224.0.0.251')
-    # ip_Graph = reader.single_ip_graph(ip_pkts)
-    # NetworkDrawer.draw_network(ip_Graph)
+#######################
+#   Main for tests    #
+#######################
 
-    # NetworkDrawer.pdfdump(reader.pcap[101], "mypacket.pdf")
-    # print "Number of nodes: " + str(reader.nodes_quantity())
+# if __name__ == '__main__':
+#     reader = PcapReader('test-data/android-Sun-Dec-25-10-42-29-GMT+02-00-2016.cap')
+#     # ip_pkts = reader.pkt_by_ip('224.0.0.251')
+#     # ip_Graph = reader.single_ip_graph(ip_pkts)
+#     # NetworkDrawer.draw_network(ip_Graph)
 
-    # NetworkDrawer.open_in_wireshark(reader.pcap)
-    s = NetworkDrawer.get_traffic_for_ip_in_time(reader.cap_path, '255.255.255.255', '2016-12-25 09:15:43')
-    print()
-    print()
-    print(s)
+#     # NetworkDrawer.pdfdump(reader.pcap[101], "mypacket.pdf")
+#     # print "Number of nodes: " + str(reader.nodes_quantity())
+
+#     # NetworkDrawer.open_in_wireshark(reader.pcap)
+#     s = NetworkDrawer.get_traffic_for_ip_in_time(reader.cap_path, '255.255.255.255', '2016-12-25 09:15:43')
+#     print()
+#     print()
+#     print(s)
