b11c06d9732b095283c5696f546fd8aa9f2fe1d6
daniel4x <daniel4x@gmail.com>
2017-01-14 20:01:38 +0200

    finished

--- PcapReader.py
+++ PcapReader.py
@@ -2,18 +2,22 @@ from scapy.all import rdpcap, Dot11
 import networkx as nx
 from NetworkDrawer import NetworkDrawer
 
-# PcapReader
-# Denotes a basic interface againts a specific given cap file
 class PcapReader:
+    """ 
+        class : PcapReader
+        Denotes a basic interface againts a specific given cap file
+    """
     
     def __init__(self, cap_path):
         self.cap_path = cap_path
         self.pcap = rdpcap(self.cap_path)
         self.graph = self.generate_network_graph()
 
-    # getByLayer
-    # return a list of packets by layer
     def getByLayer(self, layer=Dot11):
+        """
+            getByLayer
+            return a list of packets by layer
+        """
         pkts = [] # 802.11 pkts
         for pkt in self.pcap:
             if pkt.haslayer(layer):
@@ -21,23 +25,29 @@ class PcapReader:
 
         return pkts
 
-    # generate_network_graph
-    # genereates an network graph of the given pcap
     def generate_network_graph(self):
+        """
+            generate_network_graph
+            genereates an network graph of the given pcap
+        """
         self.G = nx.Graph()
         self.ip_pkts = self.getByLayer('IP')
         for pkt in self.ip_pkts:
             self.G.add_edge(pkt.src, pkt.dst)
         return self.G
 
-    # show_network
-    # shows the network graph of the given pcap
     def show_network(self):
+        """
+            show_network
+            shows the network graph of the given pcap
+        """
         NetworkDrawer.draw_network(self.graph)
 
-    # pkt_by_ip
-    # returns a list of packets by specific ip
     def pkt_by_ip(self, ip=None):
+        """
+            pkt_by_ip
+            returns a list of packets by specific ip
+        """
         is_Error = False
         if ip is None:
             print('ip address mush be specified.')
@@ -51,17 +61,21 @@ class PcapReader:
 
         return pkts_by_ip
 
-    # single_ip_graph
-    # creates a network from a given packet list
     def single_ip_graph(self, pkt_by_ip):
+        """
+            single_ip_graph
+            creates a network from a given packet list
+        """
         G = nx.Graph()
         for pkt in pkt_by_ip:
             G.add_edge(pkt.src, pkt.dst)
         return G
 
-    # nodesQuantity
-    # returns the number of nodes in the network's graph
     def nodes_quantity(self):
+        """
+            nodesQuantity
+            returns the number of nodes in the network's graph
+        """
         return nx.number_of_nodes(self.graph)
 
     def get_ip_list(self):
